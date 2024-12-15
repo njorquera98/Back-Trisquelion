00cd30114b0a62aeb806e0884c0be420533f927f
njorquera98 <njorquera98.dev@gmail.com>
2024-12-15 18:33:15 -0300

    fix(database): correccion carga de entidades en la DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PacientesModule } from './pacientes/pacientes.module';
+import { SesionesModule } from './sesiones/sesiones.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
@@ -14,14 +16,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     useFactory: (configService: ConfigService) => ({
       type: 'mysql',
       host: configService.get('DB_HOST'),
-      port: parseInt(configService.get('DB_PORT', '3306')),
+      port: parseInt(configService.get('DB_PORT')),
       username: configService.get('DB_USERNAME'),
       password: configService.get('DB_PASSWORD'),
       database: configService.get('DB_DATABASE'),
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
       autoLoadEntities: true,
       synchronize: true,
     }),
   }),
+    PacientesModule,
+    SesionesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
